17043421f0f8414848e7a874bf17f86fb093b443
ravitejaviyyuri <82799427+ravitejaviyyuri@users.noreply.github.com>
2021-07-12 15:43:18 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
           "user" : 'ubuntu',
           "host" : '172.31.3.120',
           "ref"  : 'origin/master',
-          "repo" : 'https://github.com/ravitejaviyyuri/pm2.git',
+          "repo" : 'git@github.com:ravitejaviyyuri/pm2.git',
           "path" : '/home/ubuntu/pm2-apps/ecom-react',
           "pre-deploy-local": '',
           "post-deploy" : 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --name ECOM-NEXT',
